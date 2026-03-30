b9fe4fd711f8d799b24cbfc4a7fed8425d707247
Andres-1109 <felipe11092002@gmail.com>
2026-03-30 18:12:50 -0500

    Feature to edit students implemented

--- data/menu_options.py
+++ data/menu_options.py
@@ -1,10 +1,10 @@
-from services.crud_services import add_student, delete_student
+from services.crud_services import add_student, delete_student, edit_student
 
 def menu_options():
     return [
         {"option": "Add student", "action": add_student},
         {"option": "Delete student", "action": delete_student},
-        # {"option": "Edit student", "action": edit_student},
+        {"option": "Edit student", "action": edit_student},
         # {"option": "Search student", "action": search_student},
         # {"option": "Save CSV", "action": save_csv},
         # {"option": "Upload CSV", "action": upload_csv},
--- services/crud_services.py
+++ services/crud_services.py
@@ -29,3 +29,17 @@ def delete_student(students):
         choice = validate_positive_int_range("Enter the number of the student to delete: ", number_of_students) -1
         students.pop(choice)
     return students    
+
+def edit_student(students):
+    if students == []:
+        print("There are no students to edit")
+    else:
+        number_of_students = len(students)
+        for i, student in enumerate(students, start= 1):
+            print(f'{i} . |ID: {student["id"]}| Name: {student["name"]}| Program: {student["program"]}| Status: {student["status"]}|')
+        choice = validate_positive_int_range("Enter the number of the student to delete: ", number_of_students) -1
+
+        students[choice]["name"] = validate_not_empty("Enter the name of the student: ")
+        students[choice]["program"] = validate_not_empty("Enter the program of the student: ")
+        students[choice]["status"] = validate_positive_int_range('Enter "1" for active or "2" for inactive: ', 2)
+    return students
